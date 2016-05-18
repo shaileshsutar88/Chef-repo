260cc7bbe5c016eb81688a2bca952b53a5fbb6f4
shaileshsutar88 <shaileshsutar88@gmail.com>
2016-05-18 12:21:08 +0530

    Global change
    Made changes for global use

--- cookbooks/scripts/recipes/default.rb
+++ cookbooks/scripts/recipes/default.rb
@@ -2,13 +2,13 @@
 # Cookbook Name:: scripts
 # Recipe:: default
 #
-# Copyright 2014, Neova Solutions Pvt Ltd.
+# Copyright 2014, 
 #
 # All rights reserved - Do Not Redistribute
 #
 # Author :: Shailesh Sutar
 # Write out own home page
-cookbook_file "/data/NEOVA/scripts/app_bkp.sh" do
+cookbook_file "/data/scripts/app_bkp.sh" do
         source "app_bkp.sh"
         mode "0755"
 end
