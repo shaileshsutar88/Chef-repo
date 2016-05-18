c5d1408ca85a38f6ce18d3dd24119096f663748a
shaileshsutar88 <shaileshsutar88@gmail.com>
2016-05-18 11:45:00 +0530

    Global change
    Made changes so that it can be used for global.

--- cookbooks/scripts/files/default/app_bkp.sh
+++ cookbooks/scripts/files/default/app_bkp.sh
@@ -7,15 +7,15 @@
 # Date :: 11 Feb 2014
 #
 
-FILE="vj_app`date +%d_%b_%Y`.tar.gz"
+FILE="web_app`date +%d_%b_%Y`.tar.gz"
 
-S3_BKP_PATH="s3://jmgbackup/virtualjerusalembkp/"
+S3_BKP_PATH="s3://bucket_location/folder-name"
 
-cd /data/html/
+cd /var/www/html/
 
-tar -cvjf /data/NEOVA/backups/$FILE ./
+tar -cvjf /data//backups/$FILE ./
 
-s3cmd --recursive put /data/NEOVA/backups/$FILE  $S3_BKP_PATH | tee -a /data/NEO                                                                                                                VA/logs/app-bkp.log
+s3cmd --recursive put /data/backups/$FILE  $S3_BKP_PATH | tee -a /data/                                                                                                                VA/logs/app-bkp.log
 
 #END
 
